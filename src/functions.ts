bb05af844d01a42fd494ba54436aab7bee4c8f22
Michael <mpdudas@udel.edu>
2026-02-16 14:17:56 -0500

    Completed task 4

--- src/functions.ts
+++ src/functions.ts
@@ -4,7 +4,7 @@
  *      C = (F - 32) * 5/9
  */
 export function fahrenheitToCelius(temperature: number): number {
-    return 0;
+    return (temperature - 32) * (5 / 9);
 }
 
 /**
@@ -12,7 +12,13 @@ export function fahrenheitToCelius(temperature: number): number {
  * if the number is greater than zero.
  */
 export function add3(first: number, second: number, third: number): number {
-    return 0;
+    let sum: number = 0;
+
+    if (first > 0) sum += first;
+    if (second > 0) sum += second;
+    if (third > 0) sum += third;
+
+    return sum;
 }
 
 /**
@@ -20,7 +26,8 @@ export function add3(first: number, second: number, third: number): number {
  * mark added to the end.
  */
 export function shout(message: string): string {
-    return "";
+    message = message.toUpperCase();
+    return message + "!";
 }
 
 /**
@@ -28,7 +35,8 @@ export function shout(message: string): string {
  * mark. Do not use an `if` statement in solving this question.
  */
 export function isQuestion(message: string): boolean {
-    return true;
+    let lastChar: string = message[message.length - 1];
+    return lastChar === "?";
 }
 
 /**
@@ -37,5 +45,12 @@ export function isQuestion(message: string): boolean {
  * upper or lower case), then return `false`. Otherwise, return `null`.
  */
 export function convertYesNo(word: string): boolean | null {
-    return true;
+    switch (word.toLowerCase()) {
+        case "yes":
+            return true;
+        case "no":
+            return false;
+        default:
+            return null;
+    }
 }
